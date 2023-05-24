1125a716fd95f9c3a3a6d6cbecef7aa7853091fa
kballadar21 <kevincfjh@gmail.com>
2023-05-24 09:09:27 +0200

    first commit

--- UserAccess/src/app/app.module.ts
+++ UserAccess/src/app/app.module.ts
@@ -1,28 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
-import { RegistroComponent } from './registro/registro.component';
-import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PaginaInicialComponent,
-    RegistroComponent,
-    InicioSesionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,  ],
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
